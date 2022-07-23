165289bc62709930651ce29734c4d25e62c2a5e5
18062706139 <2279549769@qq.com>
2022-07-23 15:59:24 +0800

    Promise

--- 学习笔记/前端学习/手写代码/new实现原理.js
+++ 学习笔记/前端学习/手写代码/new实现原理.js
@@ -1,25 +1,15 @@
-// 1. 首先创建一个新对象。
-// 2. 设置原型，将对象的原型「__proto__」设置为函数的prototype对象。
-// 3. 让函数的this指向这个对象，执行构造函数的代码（为这个对象添加新属性）
-// 4. 判断函数的返回值类型，如果是值类型，返回创建的对象；如果是引用类型，就返回引用类型的对象。
-
-function objectFactory() {
+function objectFactory () {
     let newObject = null;
-    let constructor = Array.prototype.shift.call(arguments);
-    let result = null;
-    if(typeof constructor !== 'function') {
-        console.log("Type error");
+    let constrcutor = Array.prototype.shift.call(arguments)
+    let result = null
+    if(typeof constrcutor != 'object') {
+        console.error('type error');
         return;
     }
-    // 新建一个空对象，对象的原型为构造函数的 prototype 对象。
-    newObject = Object.create(constructor.prototype);
-    // 将this指向新建对象，并执行函数。
-    result = constructor.apply(newObject, arguments);
-    // 判断返回对象
-    let flag = result && (typeof result == 'function' || typeof result == 'object')
-    // 判断返回结果
-    return flag ? result : newObject;
+    // 新建一个对象
+    newObject = Object.create(constrcutor.prototype)
+    // 将this指向这个函数，并执行其中的代码
+    result = constrcutor.apply(newObject, arguments)
+    let flag = result && (typeof result == 'object' || typeof result == 'function')
+    return flag ? result : newObject
 }
-
-
-objectFactory()
--- 学习笔记/前端学习/手写代码/手写代码.js
+++ 学习笔记/前端学习/手写代码/手写代码.js
@@ -41,3 +41,121 @@ function myInstanceof(left, right) {
         proto = Object.getPrototypeOf(proto)
     }
 }
+
+/**
+ * 手写 new 操作符
+ * （1）创建一个空对象
+ * （2）设置原型，将对象的原型设置为函数的prototype
+ * （3）让函数的this指向这个对象，并且执行函数的代码
+ * （4）判断函数的返回值类型，如果是值类型，
+ *      返回创建的对象，如果是引用类型，就返回这个引用类型的对象
+*/
+
+function objectFactory () {
+    let newObject = null;
+    let constructor = Array.prototype.shift.call(arguments);
+    let result = null;
+    // 判断参数是否是一个函数
+    if(typeof constructor !== "function") {
+        console.error('type error');
+        return;
+    }
+    // 新建一个对象，对象的类型为构造函数的 prototype 对象
+    newObject = Object.create(constructor.prototype);
+    // 将 this 指向这个新建对象，并执行函数
+    result = constructor.apply(newObject, arguments)
+    // 判断返回对象
+    let flag = result && (typeof result == 'object' || typeof result == 'function') 
+    // 判断返回结果
+    return flag ? result : newObject;
+}
+
+objectFactory(构造函数, 初始化参数);
+
+// 622202 3202056937385
+/**
+ * 手写 promise
+*/
+
+const PENDING = "pengding";
+const RESOLVED = "resolved";
+const REJECTED = "rejected";
+
+function MyPromise(fn) {
+    // 保存初始状态
+    var self = this;
+    // 初始化状态
+    this.state = PNEDING;
+    // 用于保存 revolve or rejected 传入的值
+    this.value = null;
+    // 用于保存 resolve 的回调函数
+    this.resolvedCallbacks = []
+    // 用于保存 reject 的回调函数
+    this.rejectedCallbacks = []
+    // 状态转变为 resolved 方法
+    function resolve(value) {
+        // 判断传入的元素是否是 Promise 值，如果是
+        // 则状态改变必须等待前一个状态改变后再进行改变
+        if(value instanceof MyPromise) {
+            return value.then(resolve, reject);
+        }
+
+        // 保证代码的执行顺序为本轮事件循环的末尾
+        setTimeout(() => {
+            // 只有状态为 pending 的时候才能转变
+            if (self.state === PENDING) {
+                // 改变状态
+                self.state = RESOLVED;
+
+                // 设置传入的值
+                self.value = value;
+
+                // 执行回调函数
+                self.resolvedCallbacks.forEach(calllback => {
+                    calllback(value);
+                });
+            }
+        }, 0);
+    }
+    function reject(value) {
+        setTimeout(() => {
+            // 只有状态为 pending 的时候才能转变
+            if(self.state === PENDING) {
+                // 改变状态
+                self.state = REJECTED;
+
+                // 设置传入的值
+                self.value = value;
+
+                // 执行回调函数
+                self.rejectedCallbacks.forEach(calllback => {
+                    calllback(value);
+                });
+
+            }
+        });
+    }
+    try {
+        fn(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+MyPromise.prototype.then = function(onResolved, onRejected) {
+    // 首先判断两个参数是否为函数类型，因为这两个参数是可选参数
+    onResolved = typeof onResolved == 'function' ? onResolved : function(value){return value;}
+    onRejected = typeof onRejected == 'function' ? onRejected : function(value){return value;}
+    if(this.state === PENDING) {
+        this.resolvedCallbacks.push(onResolved);
+        this.rejectedCallbacks.push(onRejected);
+    }
+    // 如果状态已经凝固了，则直接执行对应状态的函数
+    if(this.state === RESOLVED) {
+        onResolved(this.value)
+    }
+
+    if(this.state === REJECTED) {
+        onRejected(this.value)
+    }
+}
